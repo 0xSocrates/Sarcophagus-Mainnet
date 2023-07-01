3993c114cd8387f6e393af94acdb6b7e99b54a05
Socrates <108215275+0xSocrates@users.noreply.github.com>
2023-07-01 12:22:07 +0300

    Update sarcophagus.sh

--- sarcophagus.sh
+++ sarcophagus.sh
@@ -12,19 +12,9 @@ echo -e "\033[035mDomain Name\033[034m $Domain \033[035molarak kaydedildi"
 echo -e '\e[0m'
 echo "export Domain=$Domain" >> $HOME/.bash_profile
 sleep 1
-echo -e "\e[0;32mRPC URL seçiminizi yapın\033[0m"
-echo -e "\e[0;33m"
-sleep 1
-echo -e "1-) https://rpc.ankr.com/eth"
-echo -e "2-) https://eth.llamarpc.com"
-echo -e "3-) https://ethereum.publicnode.com"
-echo -e "4-) Özel"
-echo -e "\033[0;35m"
-read -p "Seçiminiz (1/2/3/4): " RPCSEC
-echo ""
-echo -e '\e[0m'
+RPCSEC=""
 while [[ $RPCSEC != "1" && $RPCSEC != "2" && $RPCSEC != "3" && $RPCSEC != "4" ]]; do 
-echo "\033[031mHatalı seçim yaptınız\033[0m"
+echo -e "\033[031mHatalı seçim yaptınız\033[0m"
 echo ""
 sleep 2
 echo -e "\e[0;32mRPC URL seçiminizi yapın\033[0m"
@@ -35,7 +25,7 @@ echo -e "2-) https://eth.llamarpc.com"
 echo -e "3-) https://ethereum.publicnode.com"
 echo -e "4-) Özel"
 echo -e "\033[0;35m"
-read -p "Seçiminiz (1/2/3/4): " RPCURL
+read -p "Seçiminiz (1/2/3/4): " RPCSEC
 echo ""
 echo -e '\e[0m'
 done 
@@ -61,7 +51,7 @@ cp .env.example .env
 touch peer-id.json
 Mnemonic=$(COMPOSE_PROFILES=seed docker compose run seed-gen | grep -oE "[a-zA-Z]+ [a-zA-Z]+ [a-zA-Z]+ [a-zA-Z]+ [a-zA-Z]+ [a-zA-Z]+ [a-zA-Z]+ [a-zA-Z]+ [a-zA-Z]+ [a-zA-Z]+ [a-zA-Z]+ [a-zA-Z]+")
 echo "export Mnemonic=$Mnemonic" >> $HOME/.bash_profile
-sed -i "s/^ETH_PRIVATE_KEY=.*/ETH_PRIVATE_KEY=${Privkey}/" .env
+sed -i "s/^ETH_PRIVATE_KEY=.*/ETH_PRIVATE_KEY=${PrivKey}/" .env
 sed -i "s/^DOMAIN=.*/DOMAIN=${Domain}/" .env
 sed -i "s/^PROVIDER_URL=.*/PROVIDER_URL=${RPC}/" .env
 sed -i "s/^ENCRYPTION_MNEMONIC=.*/ENCRYPTION_MNEMONIC=${Mnemonic}/" .env
@@ -71,4 +61,3 @@ echo -e "\e[0;34mKurulum Tamamlandı.\e[0m"
 echo -e "\e[0;32mArkeoloğunuzu kaydetmek için:\033[0;35m COMPOSE_PROFILES=register docker compose run register\e[0;32m komutunu çalıştırın"
 echo -e "\e[0;32mYönergeleri takip edin ardından başlatmak için :\033[0;35m COMPOSE_PROFILES=service docker compose up -d\e[0;32m komutunu çalıştırın"
 echo -e '\e[0m'
-
